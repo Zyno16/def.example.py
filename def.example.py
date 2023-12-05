a162fc66d7c7a404d6959d59c9f30f602cd23249
Zyno16 <135590226+Zyno16@users.noreply.github.com>
2023-12-05 18:38:04 +0000

    Add files via upload

---
+++ def.example.py
@@ -0,0 +1,30 @@
+def fi (name):
+    print("hello " + name)
+
+fi("ahmed")
+fi("omar")
+fi("amr")
+fi("ehab")
+print("============")
+
+def add(num1,num2):
+    print(num1+num2)
+
+add(9,6)
+print("=============")
+def calc(num3,num4,ope):
+    if ope=="+":p
+        print(num3+num4)
+    elif ope == "-":
+        print(num3 - num4)
+    elif ope =="*":
+        print(num3 * num4)
+    else:
+        if num4==0 :num4=1
+        print(num3/num4)
+
+calc(7,3,"+")
+calc(7,3,"-")
+calc(7,3,"*")
+calc(7,3,"/")
+calc(7,0,"/")
